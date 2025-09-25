aa08c32738d8ad9d2b7474af82ccfcabfdcbe9ed
Raymundo Mateos <rayma9829@gmail.com>
2025-09-25 16:30:33 -0600

    add: sort-by buttons

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -12,33 +12,73 @@ class NotesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final NoteService noteService = Provider.of<NoteService>(context);
     List<Note> notes = noteService.getNotes();
+    String currentSort = noteService.currentSortOption;
     return notes.isEmpty
         ? EmptyScreen()
         : SafeArea(
-            child: ListView.builder(
-              itemCount: notes.length,
-              itemBuilder: (context, index) {
-                final note = notes[index];
-                return ListTile(
-                  onLongPress: () {
-                    noteService.deleteNote(note.id);
-                  },
-                  title: Text(note.title ?? 'No Title'),
-                  subtitle: Text(
-                    note.content ?? 'No Content',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EditNoteScreen(note: note),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {
+                        noteService.setSortOption(
+                          currentSort == 'lastEditAsc'
+                              ? 'lastEditDesc'
+                              : 'lastEditAsc',
+                        );
+                      },
+                      label: Text('Date'),
+                      icon: Icon(
+                        currentSort == 'lastEditDesc'
+                            ? Icons.arrow_downward
+                            : Icons.arrow_upward,
                       ),
-                    );
-                  },
-                );
-              },
+                    ),
+                    TextButton.icon(
+                      onPressed: () {
+                        noteService.setSortOption(
+                          currentSort == 'titleAsc' ? 'titleDesc' : 'titleAsc',
+                        );
+                      },
+                      label: Text('Title'),
+                      icon: Icon(
+                        currentSort == 'titleDesc'
+                            ? Icons.arrow_downward
+                            : Icons.arrow_upward,
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: notes.length,
+                    itemBuilder: (context, index) {
+                      final note = notes[index];
+                      return ListTile(
+                        onLongPress: () {
+                          noteService.deleteNote(note.id);
+                        },
+                        title: Text(note.title ?? 'No Title'),
+                        subtitle: Text(
+                          note.content ?? 'No Content',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => EditNoteScreen(note: note),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           );
   }
