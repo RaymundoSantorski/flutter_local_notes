bd2f6bf8347e5be30066c7b85142a4c78273c216
Raymundo Mateos <rayma9829@gmail.com>
2025-09-25 16:01:37 -0600

    add: implementation of the empty note list screen when there are no notes

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notes/models/note.dart';
 import 'package:flutter_local_notes/models/note_service.dart';
 import 'package:flutter_local_notes/screens/edit_note_screen.dart';
+import 'package:flutter_local_notes/screens/empty_screen.dart';
 import 'package:provider/provider.dart';
 
 class NotesScreen extends StatelessWidget {
@@ -10,33 +11,35 @@ class NotesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final NoteService noteService = Provider.of<NoteService>(context);
-    List<Note> notes = noteService.notes;
-    return SafeArea(
-      child: ListView.builder(
-        itemCount: notes.length,
-        itemBuilder: (context, index) {
-          final note = notes[index];
-          return ListTile(
-            onLongPress: () {
-              noteService.deleteNote(note.id);
-            },
-            title: Text(note.title ?? 'No Title'),
-            subtitle: Text(
-              note.content ?? 'No Content',
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
+    List<Note> notes = noteService.getNotes();
+    return notes.isEmpty
+        ? EmptyScreen()
+        : SafeArea(
+            child: ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                final note = notes[index];
+                return ListTile(
+                  onLongPress: () {
+                    noteService.deleteNote(note.id);
+                  },
+                  title: Text(note.title ?? 'No Title'),
+                  subtitle: Text(
+                    note.content ?? 'No Content',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditNoteScreen(note: note),
+                      ),
+                    );
+                  },
+                );
+              },
             ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EditNoteScreen(note: note),
-                ),
-              );
-            },
           );
-        },
-      ),
-    );
   }
 }
